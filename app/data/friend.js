df9de73b860e63ebbf3317651c6098888e05b5df
m3llington <mellingtonucf@knights.ucf.edu>
2019-09-12 23:01:42 -0400

    finished work, third repository created attempt

---
+++ app/data/friend.js
@@ -0,0 +1,166 @@
+
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Captain America',
+		photo: 'https://cnet2.cbsistatic.com/img/Em3tYAnRSeSVCJH84Lvgv-fThrQ=/1600x900/2017/08/03/75c3b0ae-5a2d-4d75-b72b-055247b4378f/marvelinfinitywar-captainamerica.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Iron Man',
+		photo: 'https://nerdist.com/wp-content/uploads/2018/08/iron-mang-1088x680.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Hulk',
+		photo: 'https://img.cinemablend.com/filter:scale/quill/7/c/5/1/a/6/7c51a6800b4fd5e22fbb34311d0052734f995765.jpg?mw=600',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Black Widow',
+		photo: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/scarlett-johansson-black-widow-1561650986.jpg?crop=1.00xw:0.324xh;0,0&resize=1200:*',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Thor',
+		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-27kXRcKjj4b6PcGaN4le6rbBKfdX3677PGfYUGnWbHNAUBwxOw',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Hawkeye',
+		photo: 'https://ewedit.files.wordpress.com/2019/04/cwb0310_v005_037608.1041_r.jpg?w=2700&h=1800',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Winter Soldier',
+		photo: 'https://i.ebayimg.com/images/g/8QoAAOSw2Yta4iV-/s-l300.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Antman',
+		photo: 'https://cdn2us.denofgeek.com/sites/denofgeekus/files/styles/main_wide/public/ant-man-1-main.jpg?itok=5vvgIvMs',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'BlackPanther',
+		photo: 'https://hips.hearstapps.com/digitalspyuk.cdnds.net/18/21/1527060307-black-panther.jpg?crop=0.668xw:1.00xh;0.131xw,0&resize=480:*',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Spiderman',
+		photo: 'https://ewedit.files.wordpress.com/2018/06/tom-holland1.jpg?crop=836px%2C119px%2C1329px%2C698px&resize=1200%2C630',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
